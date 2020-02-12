1921b897c1028823b0013d344b2fbe1ba58facb4
Michael Walter <Michael_Patrick.Walter@outlook.com>
2020-02-12 15:23:41 -0800

    useDarkMode is fully funcitonal

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,12 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { useLocalStorage } from './useLocalStorage';
 
-export const useDarkMode = (key, initialValue) => {
+export const useDarkMode = (key) => {
+    const [value, setValue] = useLocalStorage(key)
+    useEffect(() => {
+        const body = document.querySelector('body');
 
-    const [someValue, setSomeValue] = useLocalStorage(key)
-
-    
-    return [storedValues, setValue];
-
+        if(value  === true){
+            return body.classList.add('dark-mode')
+            console.log('true');
+        } else {
+            return body.classList.remove('dark-mode')
+            console.log('false');
+        }
+    },[value])    
+    return [value, setValue]
 }
